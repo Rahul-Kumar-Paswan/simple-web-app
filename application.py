85dc8f1b54e8fe49a6139f35736929843f0a0fef
Rahul-Kumar-Paswan <rahulkumarpaswan941+github@gmail.com>
2025-08-02 19:51:44 +0530

    everything working fine

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return "Hello SRK!!"
+    return "Welcome To AWS Beanstalk!!"
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=8080)
